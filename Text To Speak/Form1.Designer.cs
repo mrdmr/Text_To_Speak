5f1b1083f3f35653d2223cb8d8434656c4ebdb12
mrdmr <34637640+mrdmr@users.noreply.github.com>
2017-12-24 14:26:51 +0300

    Add files via upload

---
+++ Text To Speak/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Text_To_Speak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Speak = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Resume = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.clear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // Browse
+            // 
+            resources.ApplyResources(this.Browse, "Browse");
+            this.Browse.Name = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Speak
+            // 
+            resources.ApplyResources(this.Speak, "Speak");
+            this.Speak.Name = "Speak";
+            this.Speak.UseVisualStyleBackColor = true;
+            this.Speak.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Pause
+            // 
+            resources.ApplyResources(this.Pause, "Pause");
+            this.Pause.Name = "Pause";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Resume
+            // 
+            resources.ApplyResources(this.Resume, "Resume");
+            this.Resume.Name = "Resume";
+            this.Resume.UseVisualStyleBackColor = true;
+            this.Resume.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // save
+            // 
+            resources.ApplyResources(this.save, "save");
+            this.save.Name = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            resources.ApplyResources(this.trackBar1, "trackBar1");
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Value = 100;
+            // 
+            // trackBar2
+            // 
+            resources.ApplyResources(this.trackBar2, "trackBar2");
+            this.trackBar2.Minimum = -10;
+            this.trackBar2.Name = "trackBar2";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // clear
+            // 
+            resources.ApplyResources(this.clear, "clear");
+            this.clear.Name = "clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Exit
+            // 
+            resources.ApplyResources(this.Exit, "Exit");
+            this.Exit.Name = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.trackBar1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.trackBar2);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // Stop
+            // 
+            resources.ApplyResources(this.Stop, "Stop");
+            this.Stop.Name = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.Resume);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Speak);
+            this.Controls.Add(this.Browse);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button Speak;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Resume;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ Text To Speak/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Speech.Synthesis;
+namespace Text_To_Speak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Enter text to speak";
+            Pause.Enabled = false;
+            Resume.Enabled = false;
+        }
+
+
+        SpeechSynthesizer voice = new SpeechSynthesizer();
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                ofd.CheckFileExists = true;
+                ofd.CheckPathExists = true;
+                ofd.DefaultExt = "txt";
+                ofd.DereferenceLinks = true;
+                ofd.Filter = "Text files (*.txt)|*.txt|" + "RTF files (*.rtf)|*.rtf|" +  "Works 6 and 7 (*.wps)|*.wps|" +
+                                  "Windows Write (*.wri)|*.wri|" + "WordPerfect document (*.wpd)|*.wpd";
+                ofd.Multiselect = false;
+                ofd.RestoreDirectory = true;
+                ofd.ShowHelp = true;
+                ofd.ShowReadOnly = false;
+                ofd.Title = "select a file";
+                ofd.ValidateNames = true;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    StreamReader sr = new StreamReader(ofd.OpenFile());
+                    richTextBox1.Text = sr.ReadToEnd();
+                }
+
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("can not open the file", "Text to Speak", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                 
+                
+            
+            
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            voice.Pause();
+            Resume.Enabled = true;
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            voice.Resume();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+	    
+					sfd.Filter = "All files (*.*)|*.*|wav files (*.wav)|*.wav";
+					sfd.Title = "Save to wave file";
+					sfd.FilterIndex = 2;
+					sfd.RestoreDirectory = true;
+
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                       FileStream files = new FileStream(sfd.FileName,FileMode.Create,FileAccess.Write);
+                       voice.SetOutputToWaveStream(files);
+                       voice.Speak(richTextBox1.Text);
+                       files.Close();
+                    }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+           
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            voice.SpeakAsyncCancelAll();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
